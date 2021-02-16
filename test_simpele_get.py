2f6a90e272f4b8647f9e9cd456ad88fa5658adff
renebrandenburg <renebrandenburg@gmail.com>
2021-02-16 13:58:42 +0100

    Initial commit and first testcase

---
+++ test_simpele_get.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+
+def test_simpele_get():
+    url = "http://localhost:8080/user/1"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(url, headers=headers, timeout=3)
+    assert response.status_code == 200
+    data = json.loads(response.content)
+    assert data["lastname"] == "MacDonald"
+    print(response.text)
+
+
+test_simpele_get()
