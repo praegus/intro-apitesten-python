b92663a465954689010130a973d25f21adc08e67
renebrandenburg <renebrandenburg@gmail.com>
2021-05-07 11:16:03 +0200

    1e versie loop

--- loop.py
+++ loop.py
@@ -1,21 +1,25 @@
 import json
 import requests
 users = ["1", "2", "3", "4", "5", "6", "7", "8"]
-dates = ["01", "02", "03", "04", "05", "06", "07", "08"]
+dates = [{"dateRange": "2021-03-11 tot 2021-03-14"}, {"dateRange": "2021-04-11 tot 2021-04-14"}, {"dateRange": "2021-05-11 tot 2021-05-14"}, {"dateRange": "2021-06-11 tot 2021-06-14"},
+         {"dateRange": "2021-07-11 tot 2021-07-14"}, {"dateRange": "2021-08-11 tot 2021-08-14"}, {"dateRange": "2021-09-11 tot 2021-09-14"}, {"dateRange": "2021-10-11 tot 2021-10-14"}]
 
 
 def test_post_vakantie():
     for user in users:
-        url = "http://localhost: 8080/user/reserve/{}".format(user)
-        payload = {"dateRange": "2021-03-11 tot 2021-03-14"}
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        response = requests.post(url, headers=headers, json=payload, timeout=3)
-        assert response.status_code == 200
-        data = json.loads(response.content)
-        assert data["reserveringIsGoedgekeurd"] == True
-        print(response.text)
+        url = "http://localhost:8080/user/reserve/{}".format(user)
+        for date in dates:
+            payload = date
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            response = requests.post(
+                url, headers=headers, json=payload, timeout=3)
+            assert response.status_code == 200
+            data = json.loads(response.content)
+            assert data["reserveringIsGoedgekeurd"] == True
+            print(response.text)
+            break
         break
 
 
--- test_plan_vakantie_alle_medewerkers.py
+++ test_plan_vakantie_alle_medewerkers.py
@@ -1,12 +1,14 @@
 import requests
 import json
 
+content_type = 'application/json'
+
 
 def test_maak_reservering_voor_paul():
     url = "http://localhost:8080/user/reserve/1"
     payload = {"dateRange": "2021-03-11 tot 2021-03-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -19,7 +21,7 @@ def test_maak_reservering_voor_anna():
     url = "http://localhost:8080/user/reserve/2"
     payload = {"dateRange": "2021-04-11 tot 2021-04-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -32,7 +34,7 @@ def test_maak_reservering_voor_luke():
     url = "http://localhost:8080/user/reserve/3"
     payload = {"dateRange": "2021-05-11 tot 2021-05-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -45,7 +47,7 @@ def test_maak_reservering_voor_kim():
     url = "http://localhost:8080/user/reserve/4"
     payload = {"dateRange": "2021-06-11 tot 2021-06-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -58,7 +60,7 @@ def test_maak_reservering_voor_dan():
     url = "http://localhost:8080/user/reserve/5"
     payload = {"dateRange": "2021-07-11 tot 2021-07-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -71,7 +73,7 @@ def test_maak_reservering_voor_anthony():
     url = "http://localhost:8080/user/reserve/6"
     payload = {"dateRange": "2021-08-11 tot 2021-08-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -84,7 +86,7 @@ def test_maak_reservering_voor_jake():
     url = "http://localhost:8080/user/reserve/7"
     payload = {"dateRange": "2021-09-11 tot 2021-09-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
@@ -97,7 +99,7 @@ def test_maak_reservering_voor_joanne():
     url = "http://localhost:8080/user/reserve/8"
     payload = {"dateRange": "2021-10-11 tot 2021-10-14"}
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': content_type
     }
     response = requests.post(url, headers=headers, json=payload, timeout=3)
     assert response.status_code == 200
