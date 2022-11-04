62d6b51d2f6ed9dc8bb025fb0ba034a48d09c9a4
renebrandenburg <renebrandenburg@gmail.com>
2022-11-04 11:02:48 +0100

    Setup laatste test

---
+++ test_RestFull_booker_simpele_foutieve_get.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+base_url = "http://localhost:3001"
+
+# Let op work in progress
+
+
+def test_get_niet_bestaande_booking():
+    url = base_url + "/booking/?"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(url, headers=headers, timeout=3)
+    # https://stackoverflow.com/questions/16573332/jsondecodeerror-expecting-value-line-1-column-1-char-0
+    response.raise_for_status()  # raises exception when not a 2xx response
+    if response.status_code != 204:
+        return response.json()
+    assert response.status_code == 404
+    data = json.loads(response.content)
+    assert data["error"] == "Not Found"
+    print(response.content)
+
+
+test_get_niet_bestaande_booking()
