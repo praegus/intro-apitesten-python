f99acd0e686a216a06a8430676ce69ca9970674b
renebrandenburg <renebrandenburg@gmail.com>
2023-02-21 08:53:05 +0100

    Fix test

--- test_Restfull_booker_simpele_goede_get.py
+++ test_Restfull_booker_simpele_goede_get.py
@@ -7,13 +7,15 @@ base_url = "http://localhost:3001"
 def test_get_booking():
     url = base_url + "/booking/6"
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive',
+        'Accept': '*/*'
     }
     response = requests.get(url, headers=headers, timeout=3)
     assert response.status_code == 200
 
     data = json.loads(response.content)
-    assert data["firstname"] == "Mary"
+    assert data["firstname"] == "Jim"
     print(response.content)
 
 
